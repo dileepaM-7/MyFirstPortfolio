dbf16b86a29256dd53016fc819a231c816ea58ef
Dileepa Malshan <dlpmalshan@gmail.com>
2023-11-07 23:39:16 +0530

    Added all the code.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./components/NavBar/navbar";
+function App (){
+  return(
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div><h1>heeeeeee</h1></div>
+  )
+}
+
+export default Navbar;
